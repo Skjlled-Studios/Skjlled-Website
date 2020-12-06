0b1ee19fe15dcc13aa70e9653426f89577f5a245
GreenmanYT <55301290+GreenmanYT@users.noreply.github.com>
2020-12-06 11:51:08 +1000

    e

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ function pullskeywords() {
 
 const trigger = [
   //0 
-  ["hi", "hey", "hello", "yo", "yoo", "uwu", "sup"],
+  ["hi", "hey", "hello", "yo", "yoo", "uwu", "sup", "greetings"],
   //1
   ["how are you", "how are things"],
   //2
