524054e8bcfe19698189af88e3f8ad6f1816feb3
qaz1550 <qaz1550@users.noreply.github.com>
2020-12-07 05:44:04 +1000

    styled the button moree
    now theres an annoying bump in the box that shows whenever u click it tho

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ function mise(){
         current = "-";
         //maximising
         document.getElementById("chat").style.display = "inline-block";
-        document.getElementById("chatDestroyer").style.width = "30.1%";
+        document.getElementById("chatDestroyer").style.width = "29.9%";
+        document.getElementById("chatDestroyer").style.borderTopRightRadius = "0";
     }else{
         current = "+";
         //minimising
         document.getElementById("chat").style.display = "none";
         document.getElementById("chatDestroyer").style.width = "3%";
+        document.getElementById("chatDestroyer").style.borderTopRightRadius = "50%";
     }
     document.getElementById("chatDestroyer").innerHTML=current;
 }
