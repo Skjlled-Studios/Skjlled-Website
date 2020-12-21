bd93963bc55829f7345e3023ab8287cf90cb3f87
qaz1550 <qaz1550@users.noreply.github.com>
2020-12-21 16:20:10 +1000

    animationss
    yeeeeeeeeeeeeee

--- index.js
+++ index.js
@@ -1,17 +1,43 @@
 function mise(){
     var current = document.getElementById("chatDestroyer").innerHTML;
+    var elemente = document.getElementById("chatDestroyer");
+    var chat = document.getElementById("chat");
     if (current == "+"){
         current = "-";
         //maximising
-        document.getElementById("chat").style.display = "inline-block";
-        document.getElementById("chatDestroyer").style.width = "30%";
-        document.getElementById("chatDestroyer").style.borderTopRightRadius = "0";
+        chat.style.width = "30%";
+        chat.style.height = "200px";
+        elemente.style.width = "30%";
+        elemente.style.borderTopRightRadius = "0";
+
+
+        //animation for button
+        elemente.classList.remove("buttonanimate2");
+        void elemente.offsetWidth;
+        elemente.classList.add("buttonanimate");
+
+        //animation for chat
+        chat.classList.remove("frameanimate2");
+        void chat.offsetWidth;
+        chat.classList.add("frameanimate");
     }else{
         current = "+";
         //minimising
-        document.getElementById("chat").style.display = "none";
-        document.getElementById("chatDestroyer").style.width = "3%";
-        document.getElementById("chatDestroyer").style.borderTopRightRadius = "50%";
+        chat.style.width = "0";
+        chat.style.height = "0";
+        elemente.style.width = "3%";
+        elemente.style.borderTopRightRadius = "50%";
+
+        //animation for button
+        elemente.classList.remove("buttonanimate");
+        void elemente.offsetWidth;
+        elemente.classList.add("buttonanimate2"); 
+
+        //animation for chat
+        chat.classList.remove("frameanimate");
+        void chat.offsetWidth;
+        chat.classList.add("frameanimate2"); 
+
     }
     document.getElementById("chatDestroyer").innerHTML=current;
 }
